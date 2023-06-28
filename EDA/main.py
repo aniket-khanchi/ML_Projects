b65aacffc03ddea7134642e1b9ad6b5a69eeec33
Aniket <aniket.khanchi@iu-study.org>
2023-06-29 00:22:08 +0530

    inital commit

---
+++ EDA/main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+#Reading Dataset from  http://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data
+
+
+df=pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data",sep=" ",header=None)
+headers=["Status of existing checking account","Duration in month","Credit history",\
+         "Purpose","Credit amount","Savings account/bonds","Present employment since",\
+         "Installment rate in percentage of disposable income","Personal status and sex",\
+         "Other debtors / guarantors","Present residence since","Property","Age in years",\
+        "Other installment plans","Housing","Number of existing credits at this bank",\
+        "Job","Number of people being liable to provide maintenance for","Telephone","foreign worker","Cost Matrix(Risk)"]
+df.columns=headers
+
+# df.to_csv("german_data_credit_cat.csv",index=False) #save as csv file
+
+
